f31a4b4e201ffd7c494bec63a7da63d976eb8620
Elihan303 <elihantejada@gmail.com>
2024-04-11 10:34:42 -0400

    cambio en variable de entorno 2

--- src/firebase/config/index.ts
+++ src/firebase/config/index.ts
@@ -3,15 +3,6 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBCNDRsLwkFXGq0RSD5J5XM3SeS63jSA3I",
-//   authDomain: "viasegura-4aaba.firebaseapp.com",
-//   projectId: "viasegura-4aaba",
-//   storageBucket: "viasegura-4aaba.appspot.com",
-//   messagingSenderId: "839854555961",
-//   appId: "1:839854555961:web:24c22dd270df223f005ee8",
-// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
